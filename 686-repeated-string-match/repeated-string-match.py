81888dce5a257da48ed67d1341979831efb48de0
Rajasena Reddy <rajareddy2571@gmail.com>
2026-06-05 13:20:40 +0530

    Time: 3 ms (46.18%) | Memory: 19.1 MB (95.87%) - LeetSync

---
+++ 686-repeated-string-match/repeated-string-match.py
@@ -0,0 +1,15 @@
+class Solution:
+    def repeatedStringMatch(self, a: str, b: str) -> int:
+        repeated = a
+        count = 1
+        while len(repeated) < len(b):
+            repeated += a
+            count +=1
+        if b in repeated:
+            return count 
+        
+        if b in repeated + a:
+            return count + 1
+        return -1
+
+        
