71af2d38c68630635fe8461007e85f60100a8bfe
Rajasena Reddy <rajareddy2571@gmail.com>
2026-06-10 17:20:01 +0530

    Time: 183 ms (55.08%) | Memory: 41.4 MB (95.81%) - LeetSync

---
+++ 295-find-median-from-data-stream/find-median-from-data-stream.py
@@ -0,0 +1,25 @@
+import heapq
+
+class MedianFinder:
+
+    def __init__(self):
+        self.small = []  # max heap
+        self.large = []  # min heap
+
+    def addNum(self, num):
+        heapq.heappush(self.small, -num)
+
+        if self.large and -self.small[0] > self.large[0]:
+            heapq.heappush(self.large, -heapq.heappop(self.small))
+
+        if len(self.small) > len(self.large) + 1:
+            heapq.heappush(self.large, -heapq.heappop(self.small))
+
+        if len(self.large) > len(self.small):
+            heapq.heappush(self.small, -heapq.heappop(self.large))
+
+    def findMedian(self):
+        if len(self.small) > len(self.large):
+            return -self.small[0]
+
+        return (-self.small[0] + self.large[0]) / 2.0
